2024126593c7f0e06d116b6777d73294254e3ad0
Julie <juliec@alleninstitute.org>
2019-11-08 15:47:46 -0800

    Added some basic intensity stats and a wrapper for getting them

---
+++ fov_processing_pipeline/stats.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+def z_intensity_stats(im, c):
+    ############################################
+    # For a given channel and image, calculate stats for the intensity as a function of z slice:
+    #   - Mean intensity
+    #   - Standard deviation of intensity
+    #   - Entropy of intensity
+    ############################################
+    nz = im.shape[3]
+
+    meanc = np.empty(nz)
+    stdc = np.empty(nz)
+    entropyc = np.empty(nz)
+
+    for z in range(im.shape[3]):
+        imc_vals = im[c, :, :, z].flatten()
+        meanc[z] = np.mean(imc_vals)
+        stdc[z] = np.std(imc_vals)
+
+    clabel =  'Ch'+str(c)+'_'
+    zlabel = '_by_z'
+    return {clabel+'mean'+zlabel: meanc,  clabel+'std'+zlabel: stdc}
+
+
+
+def intensity_percentiles_by_channel(im, c, percentile_list=[5, 25, 50, 75, 95]):
+    ############################################
+    # For each channel of an image, calculate the percentile intensity values in list
+    ############################################
+    results = dict()
+    imvals = im[c, :, :, :].flatten()
+    for p in percentile_list:
+        results.update({'Ch'+str(c)+'_Percentile'+str(p): np.percentile(imvals, p)})
+    return results
