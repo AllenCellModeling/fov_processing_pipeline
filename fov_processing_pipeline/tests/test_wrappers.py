c0355f4a14929a28db82b1b27551cfb39ed15fb6
Julie <juliec@alleninstitute.org>
2019-11-14 11:10:49 -0800

    Update tests for match updated function returns

--- fov_processing_pipeline/tests/test_wrappers.py
+++ fov_processing_pipeline/tests/test_wrappers.py
@@ -4,12 +4,19 @@ from .. import wrappers
 
 
 def test_row2im(demo_fov_row):
-    im = wrappers.row2im(demo_fov_row)
+    im, ch_list = wrappers.row2im(demo_fov_row)
 
     assert len(im.shape) == 4
 
+    assert len(ch_list) == 4
 
-def test_save_load_data(demo_cell_data, demo_fov_data, tmpdir, dummy_get_data):
+    assert 'BF' in ch_list
+    assert 'DNA' in ch_list
+    assert 'Cell' in ch_list
+    assert 'Struct' in ch_list
+
+
+def test_save_load_data(demo_cell_data, demo_fov_data, tmpdir): #, dummy_get_data):
     # with mock.patch(
     #     "fov_processing_pipeline.data.get_data", side_effect=dummy_get_data
     # ):
