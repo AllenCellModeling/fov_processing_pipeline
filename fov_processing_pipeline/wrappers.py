41fe9591b3cc71c8c1f7b39749fdec45d96c3d41
Julie <juliec@alleninstitute.org>
2019-11-18 13:20:04 -0800

    Fixed df indexing bug

--- fov_processing_pipeline/wrappers.py
+++ fov_processing_pipeline/wrappers.py
@@ -98,8 +98,8 @@ def load_stats(df, stats_paths):
             with open(stats_path, "rb") as f:
                 stats = pickle.load(f)
 
-            stats["FOVId"] = df.FOVId[i]
-            stats["ProteinDisplayName"] = df.ProteinDisplayName[i]
+            stats["FOVId"] = df["FOVId"].iloc[i]
+            stats["ProteinDisplayName"] = df["ProteinDisplayName"].iloc[i]
             stats_list.append(stats)
         else:
             warnings.warn("{} is missing.".format(stats_path))
