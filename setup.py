4b8a8a12c5b07d4587c7ba930fd2304f33fd6466
Gregory Johnson <gregj@alleninstitute.org>
2020-02-03 14:17:15 -0800

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -118,6 +118,6 @@ setup(
     url="https://github.com/AllenCellModeling/fov_processing_pipeline",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.1.1",
+    version="0.1.2",
     zip_safe=False,
 )
